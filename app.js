95f14433fe19f8fab808ded4ad1ca38cde534d42
fadhilahm <fadhilahmetra@gmail.com>
2020-04-19 18:35:05 +0700

    Heroku scheduler config

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-var cron = require("node-cron");
+// var cron = require("node-cron");
 const { sendEmail } = require("./helpers/axios");
 
-cron.schedule(
-  "* * * * *",
-  () => {
-    let currentTime = new Date();
-    console.log(`Current Time is ${currentTime}`);
-    let testString = `Current Time is ${currentTime}`;
-    sendEmail.post("/mail", {
-      to: "fadhilahmetra@gmail.com",
-      subject: "Sent to you by cron : " + testString,
-      html: "<p>Lorem ipsum dolor..., <b>vel</b> luctu.</p>",
-      company: "K-temuan Inc",
-      sendername: "K-temuan Inc Customer Support",
-    });
-  },
-  {
-    scheduled: true,
-    // timezone: "Asia/Jakarta",
-  }
-);
+// cron.schedule(
+//   "* * * * *",
+//   () => {
+//     let currentTime = new Date();
+//     console.log(`Current Time is ${currentTime}`);
+//     let testString = `Current Time is ${currentTime}`;
+//     sendEmail.post("/mail", {
+//       to: "fadhilahmetra@gmail.com",
+//       subject: "Sent to you by cron : " + testString,
+//       html: "<p>Lorem ipsum dolor..., <b>vel</b> luctu.</p>",
+//       company: "K-temuan Inc",
+//       sendername: "K-temuan Inc Customer Support",
+//     });
+//   },
+//   {
+//     scheduled: true,
+//     // timezone: "Asia/Jakarta",
+//   }
+// );
+
+let currentTime = new Date();
+console.log(`Current Time is ${currentTime}`);
+let testString = `Current Time is ${currentTime}`;
+sendEmail.post("/mail", {
+  to: "fadhilahmetra@gmail.com",
+  subject: "Sent to you by cron : " + testString,
+  html: "<p>Lorem ipsum dolor..., <b>vel</b> luctu.</p>",
+  company: "K-temuan Inc",
+  sendername: "K-temuan Inc Customer Support",
+});
