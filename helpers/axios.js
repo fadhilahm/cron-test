49bd4cdb3eaf00f208d2c76d839379b75db7b04b
fadhilahm <fadhilahmetra@gmail.com>
2020-04-19 17:50:18 +0700

    Success running cron on local computer

---
+++ helpers/axios.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+const sendEmail = axios.create({
+  baseURL: "https://ktemuan-e8fb.restdb.io",
+  headers: {
+    "x-apikey": "948544c49eb63b1d8defb2ce5754adabd00a5",
+  },
+});
+
+module.exports = {
+  sendEmail,
+};
